8209c6ed7c9a0eda876e4ee7b93a9f975dcfb466
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-11-21 22:13:31 +0900

    feat:restaurant screen develop oneRestaurant component

--- src/app/screens/RestaurantPage/OneRestaurant.tsx
+++ src/app/screens/RestaurantPage/OneRestaurant.tsx
@@ -1,4 +1,297 @@
+import { Badge, Box, Button, Checkbox, Container, Stack } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
+import {
+  Category,
+  Check,
+  FavoriteBorder,
+  MonetizationOn,
+} from "@mui/icons-material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import StarIcon from "@mui/icons-material/Star";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+const restaurant_list = Array.from(Array(10).keys());
+const product_list = Array.from(Array(8).keys());
+
 export function OneRestaurant() {
-  return <div>OneRestaurant</div>;
+  return (
+    <div className="single_restaurant">
+      <Container>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Stack className={"avatar_big_box"}>
+            <Box className={"top_text"}>
+              <p>Texas De Brazil Restaurant</p>
+
+              <Box className={"Single_search_big_box"}>
+                <form className={"Single_search_form"} action="" method="">
+                  <input
+                    type={"search"}
+                    className={"Single_searchInput"}
+                    name={"Single_resSearch"}
+                    placeholder={"Qidiruv"}
+                  />
+                  <Button
+                    className={"Single_button_search"}
+                    variant="contained"
+                    endIcon={<SearchIcon />}
+                  >
+                    Izlash
+                  </Button>
+                </form>
+              </Box>
+            </Box>
+          </Stack>
+
+          <Stack
+            style={{ width: "100%", display: "flex" }}
+            flexDirection={"row"}
+            sx={{ mt: "35px" }}
+          >
+            <Box className={"prev_btn restaurant-prev"}>
+              <ArrowBackIosNewIcon
+                sx={{ fontSize: 40 }}
+                style={{ color: "white" }}
+              />
+            </Box>
+
+            <Swiper
+              className={"restaurant_avatars_wrapper"}
+              slidesPerView={7}
+              centeredSlides={false}
+              spaceBetween={30}
+              navigation={{
+                nextEl: ".restaurant-next",
+                prevEl: ".restaurant-prev",
+              }}
+            >
+              {restaurant_list.map((ele, index) => {
+                return (
+                  <SwiperSlide
+                    style={{ cursor: "pointer" }}
+                    key={index}
+                    className={"restaurant_avatars"}
+                  >
+                    <img src={"/restaurant/burak1.jpeg"} />
+                    <span>Burak</span>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+            <Box
+              className={"next_btn restaurant-next"}
+              style={{ color: "white" }}
+            >
+              <ArrowForwardIosIcon sx={{ fontSize: 40 }} />
+            </Box>
+          </Stack>
+
+          <Stack
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"flex-end"}
+            width={"90%"}
+            sx={{ mt: "65px" }}
+          >
+            <Box className={"dishs_filter_box"}>
+              <Button variant={"contained"} color="primary">
+                new
+              </Button>
+              <Button variant={"contained"} color="primary">
+                price
+              </Button>
+              <Button variant={"contained"} color="primary">
+                likes
+              </Button>
+              <Button variant={"contained"} color="primary">
+                views
+              </Button>
+            </Box>
+          </Stack>
+
+          <Stack
+            style={{ width: "100%", display: "flex", minHeight: "600px" }}
+            flexDirection={"row"}
+          >
+            <Stack className={"dish_category_box"}>
+              <div className={"dish_category_main"}>
+                <Button variant={"contained"} color="primary">
+                  boshqa
+                </Button>
+                <Button variant={"contained"} color="primary">
+                  desert
+                </Button>
+                <Button variant={"contained"} color="primary">
+                  ichimlik
+                </Button>
+                <Button variant={"contained"} color="primary">
+                  salad
+                </Button>
+                <Button variant={"contained"} color="primary">
+                  ovqatlar
+                </Button>
+              </div>
+            </Stack>
+
+            <Stack className={"dish_wrapper"}>
+              {product_list.map((ele, index) => {
+                const size_volume = "normal size";
+
+                return (
+                  <Box className={"dish_box"} key={`${index}`}>
+                    <Box
+                      className={"dish_img"}
+                      sx={{ backgroundImage: `url("/others/qovurma.jpeg")` }}
+                    >
+                      <div className={"dish_sale"}>{size_volume}</div>
+
+                      <Button
+                        className={"like_view_btn"}
+                        style={{ left: "36px" }}
+                      >
+                        <Badge badgeContent={8} color="primary">
+                          <Checkbox
+                            icon={<FavoriteBorder style={{ color: "white" }} />}
+                            id={`${index}`}
+                            checkedIcon={
+                              <FavoriteBorder style={{ color: "red" }} />
+                            }
+                            checked={false}
+                          />
+                        </Badge>
+                      </Button>
+
+                      <Button className={"view_btn"}>
+                        <img
+                          src={"/icons/shopping-cart.svg"}
+                          style={{ display: "flex" }}
+                        />
+                      </Button>
+                      <Button
+                        className={"like_view_btn"}
+                        style={{ right: "36px" }}
+                      >
+                        <Badge badgeContent={1000} color="primary">
+                          <Checkbox
+                            icon={
+                              <RemoveRedEyeIcon style={{ color: "white" }} />
+                            }
+                          />
+                        </Badge>
+                      </Button>
+                    </Box>
+
+                    <Box className={"dish_desc"}>
+                      <span className={"dish_title_text"}>Shirin qovurma</span>
+                      <div className={"dish_desc_text"}>
+                        <MonetizationOnIcon />7
+                      </div>
+                    </Box>
+                  </Box>
+                );
+              })}
+            </Stack>
+          </Stack>
+        </Stack>
+      </Container>
+
+      <div className={"review_for_restaurant"}>
+        <Container
+          sx={{ mt: "100px" }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Box className={"category_title"}>Oshxona haqida fikirlar</Box>
+          <Stack
+            flexDirection={"row"}
+            display={"flex"}
+            justifyContent={"space-between"}
+            width={"100%"}
+          >
+            {Array.from(Array(4).keys()).map((ele, index) => {
+              return (
+                <Box className={"review_box"} key={index}>
+                  <Box display={"flex"} justifyContent={"center"}>
+                    <img
+                      src={"/community/cute_girl.jpg"}
+                      className={"review_img"}
+                    />
+                  </Box>
+                  <span className={"review_name"}>Rayhon Asdaova</span>
+                  <span className={"review_prof"}>Foydalanuvchi</span>
+                  <p className={"review_desc"}>
+                    Menga bu oshxonaga xardoyim kelaman,Ovqatlari juda mazzali
+                  </p>
+                  <div className={"review_stars"}>
+                    <StarIcon style={{ color: "#F2BD57" }} />
+                    <StarIcon style={{ color: "#F2BD57" }} />
+                    <StarIcon style={{ color: "#F2BD57" }} />
+                    <StarIcon style={{ color: "#whitesmoke" }} />
+                    <StarIcon style={{ color: "#whitesmoke" }} />
+                  </div>
+                </Box>
+              );
+            })}
+          </Stack>
+        </Container>
+      </div>
+
+      <Container className="member_reviews">
+        <Box className={"category_title"}>Oshxona haqida</Box>
+        <Stack
+          display={"flex"}
+          flexDirection={"row"}
+          width={"90%"}
+          sx={{ mt: "70px" }}
+        >
+          <Box
+            className={"about_left"}
+            sx={{
+              backgroundImage: `url('/restaurant/texasDeBrazil.jpeg')`,
+            }}
+          >
+            <div className={"about_left_desc"}>
+              <span>Burak</span>
+              <p>Eng mazzali oshxona</p>
+            </div>
+          </Box>
+          <Box className={"about_right"}>
+            {Array.from(Array(3).keys()).map((ele, index) => {
+              return (
+                <Box display={"flex"} flexDirection={"row"} key={index}>
+                  <div className={"about_right_img"}></div>
+                  <div className={"about_right_desc"}></div>
+                  <span>Bizning mohir oshpazlarimiz</span>
+                  <p>Bizning oshpazlarimiz o'z ishlarni ustalari,</p>
+                </Box>
+              );
+            })}
+          </Box>
+        </Stack>
+        <Stack
+          sx={{ mt: "60px" }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Box className={"category_title"}>Oshxona Manzili</Box>
+          <iframe
+            style={{ marginTop: "60px" }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2999.569811740135!2d69.30313167677262!3d41.252927704323604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae5f0136d64827%3A0x253490aa25a15247!2sMerhaba%20Steakhouse!5e0!3m2!1sen!2skr!4v1700439430993!5m2!1sen!2skr"
+            width="1320"
+            height="500"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
