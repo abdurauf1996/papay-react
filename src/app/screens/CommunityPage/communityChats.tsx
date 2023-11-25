395f066b115b7ab022a453ae69445c2a281b4234
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-11-25 22:30:22 +0900

    feat:community screen page publish

---
+++ src/app/screens/CommunityPage/communityChats.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Avatar, Box, Stack } from "@mui/material";
+import { Send } from "@mui/icons-material";
+import "../../../css/community.css";
+
+export function CommunityChats() {
+  return (
+    <Stack className="chat_frame">
+      <Box className="chat_top">Jonli Muloqot</Box>
+      <Box className="chat_content">
+        <Stack className="chat_main">
+          <Box
+            flexDirection={"row"}
+            style={{ display: "flex" }}
+            sx={{ m: "10px 0px" }}
+          >
+            <div className="msg_left">Assalomu alaykum!</div>
+          </Box>
+          <Box
+            flexDirection={"row"}
+            style={{ display: "flex" }}
+            alignItems={"flex-end"}
+            justifyContent={"flex-end"}
+            sx={{ m: "10px 0px" }}
+          >
+            <div className="msg_right">Va alleykum salom</div>
+          </Box>
+          <Box
+            flexDirection={"row"}
+            style={{ display: "flex" }}
+            sx={{ m: "10px 0px" }}
+          >
+            <Avatar alt="simon" src="/community/cute_girl.jpg" />
+            <div className="msg_left">MIT6 gurhimizga xush kelibsiz!</div>
+          </Box>
+          <Box
+            flexDirection={"row"}
+            style={{ display: "flex" }}
+            alignItems={"flex-end"}
+            justifyContent={"flex-end"}
+            sx={{ m: "10px 0px" }}
+          >
+            <div className="msg_right">
+              Bizga berga ishonchlaring uchun rahmat
+            </div>
+          </Box>
+        </Stack>
+      </Box>
+      <Box className="chat_bott">
+        <input
+          type="text"
+          name="message"
+          className="msg_input"
+          placeholder="Xabar jo'natish"
+        />
+        <button className="send_msg_btn">
+          <Send style={{ color: "#fff" }} />
+        </button>
+      </Box>
+    </Stack>
+  );
+}
--- src/app/screens/CommunityPage/index.tsx
+++ src/app/screens/CommunityPage/index.tsx
@@ -1,5 +1,93 @@
-import React from "react";
-import { Container } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Container, PaginationItem, Stack } from "@mui/material";
+import Tab from "@mui/material/Tab";
+import Pagination from "@mui/material/Pagination";
+import "../../../css/community.css";
+import { TargetArticles } from "./targetArticles";
+import { CommunityChats } from "./communityChats";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import { TabPanel } from "@mui/lab";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const targetBoArticles = [1, 2, 3, 4];
 export function CommunityPage() {
-  return <Container>CommunityPage</Container>;
+  /** INITIALIZATION **/
+  const [value, setValue] = useState("1");
+
+  /** HANDLERS *****/
+  const handleChange = (event: any, newValue: string) => {
+    setValue(newValue);
+  };
+  const handlePaginationChange = (event: any, value: number) => {
+    console.log(value);
+  };
+
+  return (
+    <div className="community_page">
+      <div className="community_frame">
+        <Container sx={{ mt: "50px", mb: "50px" }}>
+          <Stack flexDirection={"row"} justifyContent={"space-between"}>
+            <CommunityChats />
+            <Stack
+              className="community_all_frame"
+              inputMode={"text"}
+              style={{ border: "1px solid red" }}
+            >
+              <TabContext value={value}>
+                <Box className="article_tabs">
+                  <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                    <TabList
+                      onChange={handleChange}
+                      aria-label="lab API tabs example"
+                      style={{ borderColor: "blue" }}
+                    >
+                      <Tab label="Barcha Maqolalar" value="1" />
+                      <Tab label="Mashhurlar" value="2" />
+                      <Tab label="Oshxonaga Baho" value="3" />
+                      <Tab label="Hikoyalar" value="4" />
+                    </TabList>
+                  </Box>
+                </Box>
+
+                <Box className="article_main">
+                  <TabPanel value="1">
+                    <TargetArticles targetBoArticles={[1, 2]} />
+                  </TabPanel>
+                  <TabPanel value="2">
+                    <TargetArticles targetBoArticles={[1, 2, 3]} />
+                  </TabPanel>
+                  <TabPanel value="3">
+                    <TargetArticles targetBoArticles={[1, 2, 3, 4]} />
+                  </TabPanel>
+                  <TabPanel value="4">
+                    <TargetArticles targetBoArticles={[1, 2, 3, 4, 5]} />
+                  </TabPanel>
+                </Box>
+
+                <Box className="article_bott">
+                  <Pagination
+                    count={4}
+                    page={1}
+                    renderItem={(item) => (
+                      <PaginationItem
+                        components={{
+                          previous: ArrowBackIcon,
+                          next: ArrowForwardIcon,
+                        }}
+                        {...item}
+                        color="secondary"
+                      />
+                    )}
+                    onChange={handlePaginationChange}
+                  />
+                </Box>
+              </TabContext>
+            </Stack>
+          </Stack>
+        </Container>
+      </div>
+    </div>
+  );
 }
